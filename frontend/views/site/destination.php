59570b9d87e04383ef32974c85967ee283c4f12d
Hazrat Ali <iloveyii@yahoo.com>
2017-02-16 21:44:43 +0100

    carousel

--- frontend/views/site/destination.php
+++ frontend/views/site/destination.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
-use tuyakhov\youtube\EmbedWidget;
+use yii\bootstrap\Carousel;
 
 $this->title = 'Destinations ';
 $this->params['breadcrumbs'][] = ['label'=>'Movies', 'url'=>['site/movies']];
@@ -21,14 +21,38 @@ $this->params['breadcrumbs'][] = $this->title;
 
     $c = 0;
 ?>
-
 <div class="newspaper">
     <?php foreach($trips as $trip): ?>
         <div class="list-group">
             <a href="<?=$trip->link?>">
-                <img style="height:220px; width:100%" src="<?=$trip->image_link?>" class="img-thumbnail" />
+                <?php
+                $csvImages = '';
+                $aImages = [];
+                $aImages[] = [
+                    'content' => "<img class='img-thumbnail' style='height:220px; width:100%' src='{$trip->image_link}'/>",
+                    'caption' => "<h4>{$trip->title}</h4><p>This is the caption text</p>",
+                    'options' => [],
+                ];
+                if(isset($trip->additional_image_link)) {
+                    $images = explode(',', $trip->additional_image_link);
+
+                    foreach($images as $image) {
+                        $aImages[] = [
+                            'content' => "<img class='img-thumbnail' style='height:220px; width:100%' src='{$image}'/>",
+                            'caption' => "<h4>{$trip->title}</h4><p>This is the caption text</p>",
+                            'options' => [],
+                        ];
+                    }
+                }
+
+                echo Carousel::widget([
+                    'items' => $aImages,
+                    'controls'=>count($aImages) > 1 ? false : false,
+                    'showIndicators'=>true,
+                ]);
+                ?>
             </a>
         </div>
-        <?php $c++; if($c > 34) break;?>
+        <?php $c++; if($c > 9) break;?>
     <?php endforeach; ?>
 </div>
