2ae7586e7f91a0d4d918453531f9eea3b1268ce5
Hazrat Ali <iloveyii@yahoo.com>
2017-02-16 11:44:44 +0100

    3 columns with equal height

--- frontend/views/site/destination.php
+++ frontend/views/site/destination.php
@@ -10,10 +10,22 @@ $this->params['breadcrumbs'][] = ['label'=>'Movies', 'url'=>['site/movies']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<?php foreach($trips as $trip): ?>
-    <div class="col-md-3 col-sm-6 list-group">
-        <a href="<?=$trip->link?>">
-            <img style="height:220px; width:100%" src="<?=$trip->image_link?>" class="img-thumbnail" />
-        </a>
-    </div>
-<?php endforeach; ?>
+<?php
+    $this->registerCss('
+        .newspaper {
+            -webkit-columns: 100px 3; /* Chrome, Safari, Opera */
+            -moz-columns: 100px 3; /* Firefox */
+            columns: 100px 3;
+        }
+    ');
+?>
+
+<div class="newspaper">
+    <?php foreach($trips as $trip): ?>
+        <div class="list-group">
+            <a href="<?=$trip->link?>">
+                <img style="height:220px; width:100%" src="<?=$trip->image_link?>" class="img-thumbnail" />
+            </a>
+        </div>
+    <?php endforeach; ?>
+</div>
